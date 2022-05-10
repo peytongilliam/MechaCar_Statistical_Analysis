3889bec6ee29dd623ee70a7320f22b08552998cb
Peyton Gilliam <bryant.peyton@yahoo.com>
2022-05-09 21:05:39 -0500

    Update RScript

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,4 @@
+#Deliverable 1
 #1.Download the MechaCar_mpg.csv file, and place it in the active directory for your R session.
 #2.Create a new RScript in your R source pane, name it MechaCarChallenge.RScript, and save it to your active directory.
 #3.Use the library() function to load the dplyr package.
@@ -7,3 +8,20 @@ MechaCar_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactor
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_table)
 #6.Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_table))
+
+#Deliverable 2
+#1.Download the Suspension_Coil.csv file, and place it in the active directory for your R session.
+#2.In your MechaCarChallenge.RScript, import and read in the Suspension_Coil.csv file as a table.
+SuspensionCoil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+#3.Write an RScript that creates a total_summary dataframe using the summarize() function to get the mean, median, variance, and standard deviation of the suspension coil's PSI column.
+total_summary <- SuspensionCoil_table %>% summarize(Mean_PSI=mean(PSI),
+                                          Median_PSI=median(PSI),
+                                          Var_PSI=var(PSI),
+                                          Std_Dev_PSI=sd(PSI),
+                                          Num_Coil=n(), .groups = 'keep')
+#4.Write an RScript that creates a lot_summary dataframe using the group_by() and the summarize() functions to group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil's PSI column.
+lot_summary <- SuspensionCoil_table  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),
+                                                                         Median_PSI=median(PSI),
+                                                                         Var_PSI=var(PSI),
+                                                                         Std_Dev_PSI=sd(PSI),
+                                                                         Num_Coil=n(), .groups = 'keep')
